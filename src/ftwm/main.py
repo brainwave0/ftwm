6fa2cde9313da73dd0b2970bef7002a517107354
Ian Campos <iancampos.0@protonmail.com>
2022-08-01 10:54:40 -0700

    implement pan function

--- src/ftwm/main.py
+++ src/ftwm/main.py
@@ -1,12 +1,21 @@
 from operator import add
-from typing import Tuple, Iterable
+from typing import TypeVar, Iterable
+T = TypeVar('T')
 
 
 class Window:
-    def __init__(self, position: Tuple[int, int] = (0, 0)):
-        self.position = position
+    def __init__(self, virtual_position: tuple[int, int] = (0, 0)):
+        self.virtual_position = virtual_position
+        self.position = (0, 0)
 
 
-def pan(windows: Iterable[Window], delta: Tuple[int, int], scale: float = 1) -> None:
+def pan(windows: Iterable[Window], delta: tuple[int, int], scale: float = 1) -> None:
+    """
+    Pans windows.
+
+    delta: How far to move each window from their virtual positions.
+    scale: Multiplied with delta to affect panning sensitivity.
+    """
     for window in windows:
-        tuple(map(add, window.position, delta))
+        window.position = (
+            int(window.virtual_position[0] + delta[0] * scale), int(window.virtual_position[1] + delta[1] * scale))
--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,21 @@
 from ftwm.main import *
 
 
-def test_pan_windows():
-    windows = [Window(position=(0, 0))]
+def test_pan():
+    windows = [Window(virtual_position=(0, 0))]
     pan(windows, (-1, 1), scale=2)
-    assert windows[0].position == (0, 0)
+    assert windows[0].position == (-2, 2)
+
+    windows = [Window(virtual_position=(1, -1))]
+    pan(windows, (0, 1), scale=1)
+    assert windows[0].position == (1, 0)
+
+    windows = [Window(virtual_position=(2, -3)),
+               Window(virtual_position=(2, -1))]
+    pan(windows, (-2, -1), scale=2)
+    assert windows[0].position == (-2, -5)
+    assert windows[1].position == (-2, -3)
+
+    windows = [Window(virtual_position=(3, -1))]
+    pan(windows, (0, 0), scale=2)
+    assert windows[0].position == (3, -1)
